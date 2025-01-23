174ac5ca4a8fb37159db256a7b3df702f134e289
RakanZah <133409235+RakanZah@users.noreply.github.com>
2025-01-23 12:15:41 +0300

    security purposes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+// Load environment variables from the `.env` file
+dotenv.config();
 
 const nextConfig: NextConfig = {
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  },
   /* config options here */
 };
 
